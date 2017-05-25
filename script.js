138704850ce49e2e7c4d91ba76ce794b066d90fb
Halim <halim.bouguedra@gmail.com>
2017-05-25 19:27:18 +0100

    change sound files

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("hello world!");
+var panel = document.getElementById("panel");
+var qs= [];
+var sounds = [];
+var soundURLs = [ 	
+		"sound/b.mp3", 
+		"sound/c.mp3", 
+		"sound/e.mp3", 
+		"sound/g.mp3"
+	];
+
+function init() {
+	// inflate the quarter divs
+	for(let i = 0; i < 4; i++) {
+		var id = "q" + i;
+		var quarter = "<div id='" + id + "' class='quarter'></div>";
+		panel.innerHTML += quarter;
+	}
+
+	// bind sounds and set bg color
+	for(var i = 0; i < 4; i++) {
+		// refs
+		var q = document.getElementById("q" + i);
+		qs.push(q);
+
+		//sounds
+		var sound = new Audio(soundURLs[i]);
+		sounds.push(sound);
+		q.onclick =( function(x){
+			return function() {
+				sounds[x] = new Audio(soundURLs[x]);
+				sounds[x].play()
+			}
+		})(i);
+	}
+
+}
+
+function playSound(i) {
+}
+init();
+
+	
